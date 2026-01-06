048cfde3cc1c1e1dc99638d05d48899f88ef914c
Harshita Melinmani <harshitamelinmani@gmail.com>
2026-01-06 20:24:10 +0530

    Student Course Registration System

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "login.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "login.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "register.html"));
+});
+
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "home.html"));
+});
+
+// Server start
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
